c6007f2c271fc022b84c732543504f390a1f2e77
Pakapol Tanachaisith <pakapol@Pakapols-Air.lan>
2024-11-20 10:57:58 +0700

    Create flights filter form

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,7 @@
 class FlightsController < ApplicationController
   def index
     @flights = Flight.all.includes(:departure_airport, :arrival_airport)
+    @airport_options = Airport.pluck(:iata, :id)
+    @flight_date_options = Flight.pluck(:depart_at).map { |item| [ item.strftime("%d/%m/%Y"), item.to_date ] }
   end
 end
