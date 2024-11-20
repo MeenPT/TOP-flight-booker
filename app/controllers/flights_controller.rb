387870ece4bb7c2291d809972ba301e7054414d6
Pakapol Tanachaisith <meen260600@gmail.com>
2024-11-20 15:59:06 +0700

    Make flights filter form functional

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -7,6 +7,21 @@ class FlightsController < ApplicationController
     @flight_date_options = flight_dates.unshift([ "Any", nil ])
 
     @flights = Flight.all.includes(:departure_airport, :arrival_airport)
+
+    if params[:from].present?
+      @flights = @flights.where(departure_airport_id: params[:from])
+    end
+
+    if params[:to].present?
+      @flights = @flights.where(arrival_airport_id: params[:to])
+    end
+
+    if params[:depart_date].present?
+      day_begin = DateTime.parse(params[:depart_date]).beginning_of_day
+      day_end = DateTime.parse(params[:depart_date]).end_of_day
+
+      @flights = @flights.where(depart_at: day_begin...day_end)
+    end
   end
 
   def show
