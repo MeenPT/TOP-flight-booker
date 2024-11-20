e6f9e6032b23417d8042b3e69001863e83fea27a
Pakapol Tanachaisith <meen260600@gmail.com>
2024-11-20 15:16:13 +0700

    Display a list of flights on the home page

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,7 +1,15 @@
 class FlightsController < ApplicationController
   def index
+    airport_codes =  Airport.pluck(:iata, :id)
+    @airport_options = airport_codes.unshift([ "Any", nil ])
+
+    flight_dates = Flight.pluck(:depart_at).map { |item| [ item.strftime("%d/%m/%Y"), item.to_date ] }
+    @flight_date_options = flight_dates.unshift([ "Any", nil ])
+
     @flights = Flight.all.includes(:departure_airport, :arrival_airport)
-    @airport_options = Airport.pluck(:iata, :id)
-    @flight_date_options = Flight.pluck(:depart_at).map { |item| [ item.strftime("%d/%m/%Y"), item.to_date ] }
+  end
+
+  def show
+    @flight = Flight.find(params[:id])
   end
 end
