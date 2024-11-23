bff35be69d15670ac0866e8e54a9955abae44887
Pakapol Tanachaisith <meen260600@gmail.com>
2024-11-23 15:36:34 +0700

    Build errors pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  match "/404", to: "errors#not_found", via: :all
+  match "/500", to: "errors#internal_server_error", via: :all
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
