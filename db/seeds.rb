4a4f3945601be299fc80ca29679e8bc0a7ac9fbe
Pakapol Tanachaisith <meen260600@gmail.com>
2024-12-18 14:13:06 +0700

    Seed flights table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,37 @@ iata_codes = [
   "BOS", "DTW", "PHL", "FLL", "SAN"
 ]
 
-iata_codes.each do |iata|
+airports = iata_codes.map do |iata|
   Airport.find_or_create_by!(iata: iata)
+end
+
+def random_flight_duration
+  hours = rand(1...15).hours
+  minutes = rand(1...59).minutes
+
+  (hours + minutes).iso8601
+end
+
+def random_flight_start
+  now = DateTime.now
+  next_year = now + 365 # 365 days from now
+  random_timestamp = rand(now.to_time.to_i..next_year.to_time.to_i)
+  Time.at(random_timestamp).to_datetime
+end
+
+airports.each do |airport|
+  destinations = airports.reject { |item| item == airport }
+
+  airport.departing_flights.create([
+    {
+      start: random_flight_start,
+      duration: random_flight_duration,
+      arrival_airport_id: destinations.sample.id
+    },
+    {
+      start: random_flight_start,
+      duration: random_flight_duration,
+      arrival_airport_id: destinations.sample.id
+    }
+  ])
 end
