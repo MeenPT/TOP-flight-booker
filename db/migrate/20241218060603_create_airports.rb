ed9e8289c9a4c0dc113022f4465fde1b817c7c00
Pakapol Tanachaisith <meen260600@gmail.com>
2024-12-18 13:10:53 +0700

    Create the Airport model and seed the airports table

---
+++ db/migrate/20241218060603_create_airports.rb
@@ -0,0 +1,9 @@
+class CreateAirports < ActiveRecord::Migration[8.0]
+  def change
+    create_table :airports do |t|
+      t.string :iata, null: false, index: { unique: true }
+
+      t.timestamps
+    end
+  end
+end
