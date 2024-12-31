20c6c82b332a715454aeeaddfc673b7e189e6e25
Pakapol Tanachaisith <meen260600@gmail.com>
2024-12-31 14:19:42 +0700

    Build flights filter form

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,7 @@
 class FlightsController < ApplicationController
   def index
     @flights = Flight.includes(:departure_airport, :arrival_airport).order(start: :asc)
+
+    @flight_dates = @flights.map { |item| item.formatted_flight_date }.uniq
   end
 end
