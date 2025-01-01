823025e9eee7e78c249f3dfd818e7ab5f71f5d83
Pakapol Tanachaisith <meen260600@gmail.com>
2025-01-01 12:24:45 +0700

    Build the flights filtering logic

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,7 +1,23 @@
 class FlightsController < ApplicationController
   def index
-    @flights = Flight.includes(:departure_airport, :arrival_airport).order(start: :asc)
+    filter_condition = {}
 
-    @flight_dates = @flights.map { |item| item.formatted_flight_date }.uniq
+    if params[:departure_airport].present?
+      filter_condition[:departure_airport_id] = params[:departure_airport]
+    end
+
+    if params[:arrival_airport].present?
+      filter_condition[:arrival_airport_id] = params[:arrival_airport]
+    end
+
+    if params[:date].present?
+      date = Date.parse(params[:date])
+
+      filter_condition[:start] = (date.beginning_of_day)..(date.end_of_day)
+    end
+
+    @flights = Flight.where(filter_condition).includes(:departure_airport, :arrival_airport).order(start: :asc)
+
+    @flight_dates = Flight.pluck(:start).map { |item| item.strftime("%Y-%m-%d") }
   end
 end
