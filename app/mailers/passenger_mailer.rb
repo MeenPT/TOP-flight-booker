69385a0941658df10bb49ae9aed1ee0f4ff99f76
Pakapol Tanachaisith <meen260600@gmail.com>
2025-01-05 10:29:44 +0700

    Send booking confirmation email to passengers

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,9 @@
 class PassengerMailer < ApplicationMailer
+  default from: "flightbooker@example.com"
+
+  def confirmation_email
+    @passenger = params[:passenger]
+    @flight = params[:flight]
+    mail(to: @passenger.email, subject: "Booking Confirmation")
+  end
 end
